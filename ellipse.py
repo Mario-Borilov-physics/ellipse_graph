6d49d354c60f6eb7d16ac6614c6d8729831d973b
Mario-Borilov-physics <mariob6a@gmail.com>
2026-05-06 17:56:44 +0300

    Add files via upload

---
+++ ellipse.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.linspace(-7,7,900)
+def func1(a,b,x):
+    return b*np.sqrt(1-pow(x/a,2))
+def func2(a,b,x):
+    return -b*np.sqrt(1-pow(x/a,2))
+y1=func1(7,3,x)
+y2=func2(7,3,x)
+fig,ax=plt.subplots()
+ax.set_ylim(-3.5,3.5)
+ax.set_xlim(-7.5,7.5)
+ax.set_xlabel('x')
+ax.set_title('ellipse')
+plt.axhline(0,color='red',linestyle='--')
+plt.axvline(0,color='red',linestyle='--')
+plt.plot(x,y1,color='orange')
+plt.plot(x,y2,color='orange')
+
+plt.show()
+
+  
